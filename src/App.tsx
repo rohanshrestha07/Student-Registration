87d8119f1206943f30006d76d44c5fdcf53eeea5
Rohan Shrestha <rohanxtha2060@gmail.com>
2024-07-28 21:21:22 +0545

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-xl font-bold text-red-600 '>Wlcome to tailwindcss with react and vite</h1>
+    </>
+  )
+}
+
+export default App
